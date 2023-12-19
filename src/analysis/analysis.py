7d0e532e739c11f72e6533e5f4035463ec75558c
Kirill Antonov <kirant9797@gmail.com>
2023-12-19 09:42:19 +0100

    change linear dependency check

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -1,19 +1,5 @@
-from sympy import Matrix
 import numpy as np
 
 def linear_dependency(vectors):
-    n = len(vectors)
-    m = stack_vectors(vectors)
-    mr, indexes = m.T.rref()
-
-    if len(indexes) == n:
-        return False
-    else:
-        return True
-
-def stack_vectors(vecs):
-    m = Matrix(vecs[0])
-    for vec in vecs[1:]:
-        m = m.row_join(Matrix(vec))
-    return m
-
+    M = np.array(vectors)
+    return np.linalg.matrix_rank(M @ M.T) != len(vectors)
