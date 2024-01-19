aaf9dd85bdfe13169777ea00c0edd4e79254e254
Kirill Antonov <kirant9797@gmail.com>
2024-01-19 01:43:39 +0100

    update running script

--- src/experiments/run-exp.sh
+++ src/experiments/run-exp.sh
@@ -4,7 +4,7 @@ for alg in "canonical-ea"
 do 
     for lambda in 1 500
     do
-        if [ "$str" == "canonical-ea" -a $lambda -eq 1 ]; then
+        if [ "$alg" == "canonical-ea" -a $lambda -eq 1 ]; then
             continue
         fi
         for degree in 10 20 50 100
@@ -13,11 +13,11 @@ do
             do
                 for (( instance=0; instance<5; instance++ ))
                 do
+                    #echo Exp_${timestamp}/A-${alg}_L${lambda}_D${degree}_C-${constant}_$timestamp
                     $(nohup python $python_script --instance $instance --algorithm $alg --dirname Exp_${timestamp}/A-${alg}_L${lambda}_D${degree}_C-${constant}_$timestamp --degree $degree --constant $constant --lmbda $lambda > /dev/null &)
                 done
             done
         done
     done
 done
-ps aux | grep $python_script | grep -v grep | wc -l
-ps aux | grep $python_script | grep -v grep
+ps aux | grep large_scale_poly_2.py | grep -v grep | wc -l
